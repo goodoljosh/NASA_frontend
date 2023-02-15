667598554bc2dcfd872131483689944c8a4c53a8
goodoljosh <findley_joshua1@columbusstate.edu>
2023-02-15 00:37:09 +0000

    Used get to receive data from backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
+import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const[data, setData] = React.useState(null);
+  
+  // React.useEffect(()=> {
+  //   fetch('/api')
+  //   .then((res) => res.json())
+  //   .then((data) => setData(data.message));
+  // }, []);
 
-  React.useEffect(()=> {
-    fetch('/api')
-    .then((res) => res.json())
-    .then((data) => setData(data.message));
-  }, []);
+  // App.get("/", function(req, res)  {
+   axios.get('https://nasa-backend.onrender.com' ).then(function(response){
+     console.log(response);
+   })
+//  });
 
   return (
     <div className="App">
