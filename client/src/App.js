2e5b45cb3031373dab85db3a45c954b56e107c28
goodoljosh <findley_joshua1@columbusstate.edu>
2023-02-21 23:18:56 +0000

    Edited mock response.

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
-const mockNasaResp = "https://3001-csuwebdev-nasabackend-4adrwz2qhf4.ws-us87.gitpod.io/";
+
 
 function App() {
   const[data, setData] = React.useState(null);
-
+  const mockNasaResp = "{\"copyright\":\"Tara Mostofi\",\"date\":\"2023-02-21\",\"explanation\":\"They are both falling. The water in Yosemite Falls, California, USA, is falling toward the Earth. Comet ZTF is falling toward the Sun. This double cosmic cascade was captured late last month as fading Comet C/2022 E3 (ZTF) had just passed its closest to planet Earth. The orange star just over the falls is Kochab. With the exception of a brief encounter with a black bear, the featured image was a well-planned composite of a moonlit-foreground and long-duration background exposures - all designed to reconstruct a deep version of an actual single sight. Although Comet ZTF is now fading as it glides back to the outer Solar System, its path is determined by gravity and so it can be considered to still be falling toward the Sun -- but backwards.    Comet ZTF Gallery: Notable Submissions to APOD\",\"hdurl\":\"https://apod.nasa.gov/apod/image/2302/CometZtfYosemite_Mostofi_1639.jpg\",\"media_type\":\"image\",\"service_version\":\"v1\",\"title\":\"Comet ZTF  over Yosemite Falls\",\"url\":\"https://apod.nasa.gov/apod/image/2302/CometZtfYosemite_Mostofi_960.jpg\"}";
+  const nasaResponse = JSON.parse(mockNasaResp);
   // React.useEffect(()=> {
   //   fetch('/api')
   //   .then((res) => res.json())
@@ -23,18 +24,18 @@ function App() {
     var config = {
       headers: {credentials: "include" }
     };
-    axios.get("https://3001-csuwebdev-nasabackend-m2liyfnaxnj.ws-us87.gitpod.io", config).then((response) => {
-    })
+    // axios.get("https://3001-csuwebdev-nasabackend-m2liyfnaxnj.ws-us87.gitpod.io", config).then((response) => {
+    //    mockNasaResp.image;
+    // })
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          {nasaResponse.title}
           {!data ? "Loading..." : data}
         </p>
-        <p>
-        {mockNasaResp.url}
-        </p>
+        
       </header>
     </div>
   );
