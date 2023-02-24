d3f54d37fc5f1187ec8c0da6a29d46682732afdf
goodoljosh <findley_joshua1@columbusstate.edu>
2023-02-24 00:13:02 +0000

    Editing nav

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 //import axios from "axios";
 import Header from "./components/Header";
 import Nav  from "./components/Nav";
+// import './Nav.css';
 import './App.css';
 
 
