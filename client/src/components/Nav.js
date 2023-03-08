13ea2684e3e185d81e42bf541044d095930cede0
goodoljosh <findley_joshua1@columbusstate.edu>
2023-03-08 00:25:14 +0000

    Added link to mars rover link.

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -5,7 +5,7 @@ const Nav = () => {
             <ul>
              <li><a >Home</a></li>
              <li><a >Picture of the Day</a></li>
-             <li><a >Mars Rover Photos</a></li>
+             <li><a href="https://nasa-backend.onrender.com/mars" >Mars Rover Photos: </a></li>
              <li><a >About</a></li>
             </ul>
     </nav>
