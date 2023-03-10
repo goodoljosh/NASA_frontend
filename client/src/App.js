eb2dd6535fefeb22b7023f008b1afd7145e8b766
goodoljosh <findley_joshua1@columbusstate.edu>
2023-03-10 00:19:55 +0000

    Added Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,31 @@
-import React, {useState, useEffect} from "react";
-import axios from "axios";
-import Header from "./components/Header";
+// import React, {useState, useEffect} from "react";
+// import axios from "axios";
+// import Header from "./components/Header";
 import Nav  from "./components/Nav";
 // import './Nav.css';
 import './App.css';
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [nasaParse, setNasaParse] = useState({})
-    useEffect(()=>{
-    //Variable for testing mars api
-    const marsDate = new Date("2015/03/04");
-    console.log(marsDate);
+  //   //Variable for testing mars api
+  //   const marsDate = new Date("2015/03/04");
+  //   console.log(marsDate);
      
-    axios.get('https://nasa-backend.onrender.com/npod' ).then(function(response){
-      setNasaParse(JSON.parse(response.data))
-    // axios.get('https://nasa-backend.onrender.com/mars' ).then(function(response){
-    //     setNasaParse(JSON.parse(response.data))
-    })
-  },[])
-   
+  //   // axios.get('https://nasa-backend.onrender.com/npod' ).then(function(response){
+  //   //   setNasaParse(JSON.parse(response.data))
+  //   // axios.get('https://nasa-backend.onrender.com/mars' ).then(function(response){
+  //   //     setNasaParse(JSON.parse(response.data))
+  //   })
+  // },[])
+
+  
 
 
 
-
-
-   
-
-    var config = {
-      headers: {credentials: "include" }
-    };
-    // axios.get("https://3001-csuwebdev-nasabackend-m2liyfnaxnj.ws-us87.gitpod.io", config).then((response) => {
-    //    mockNasaResp.image;
-    // })
   return (
     <div className="App">
         <Nav />
-        <Header nasaResponse={nasaParse}/>
-      <footer>
-        <p>©</p>
-        {nasaParse.copyright}
-      </footer>
+        <Outlet/>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PictureoftheDay from './routes/PictureoftheDay';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        {/* <Route path ="home" element = {<Home/>} /> */}
+        <Route path ="pod" element = {<PictureoftheDay/>} />
+        {/* <Route path="mars" element={<MarsPhotos />} />
+        <Route path="about" element={<About />} /> */}
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
