2b32253bbdf7c220683fc8a8d35ca9da3dbf4d3d
goodoljosh <findley_joshua1@columbusstate.edu>
2023-03-30 23:35:38 +0000

    Cypress test

--- client/cypress/integration/app-spec.js
+++ client/cypress/integration/app-spec.js
@@ -11,7 +11,7 @@ describe('example to-do app', () => {
         // We use the `cy.get()` command to get all elements that match the selector.
         // Then, we use `should` to assert that there are two matched items,
         // which are the two default items.
-        cy.get('.App h1').should('have.text', 'Brownies Are Cookies')
+        cy.get('.App h1').should('have.text', 'NASA API Project')
     })
 
 })
--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ function App() {
   return (
     <div className="App">
       <header>
-      <h1> NASA API Project</h1>
+      <h1>NASA API Project</h1>
       </header>
         <Nav />
         <Outlet/>
