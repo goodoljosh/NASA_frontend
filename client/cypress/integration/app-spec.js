3742fc57427a486c8ca8634c3d5d6aaabd93c434
goodoljosh <findley_joshua1@columbusstate.edu>
2023-04-05 21:15:43 +0000

    Testing webpage elements.

--- client/cypress/integration/app-spec.js
+++ client/cypress/integration/app-spec.js
@@ -11,7 +11,26 @@ describe('example to-do app', () => {
         // We use the `cy.get()` command to get all elements that match the selector.
         // Then, we use `should` to assert that there are two matched items,
         // which are the two default items.
+        cy.url().should('include', '/')
         cy.get('.App h1').should('have.text', 'NASA API Project')
     })
 
+    // beforeEach(() => {
+    //   cy.visit('http://localhost:3000/pod')
+    // })
+    // it('testing pod header, image, and url ', () => {    
+    //  cy.url().should('include', '/pod')
+    //  //Since data changes daily, test cases would change too
+    //  cy.get('.App-header h1').should('have.text', "Rubin's Galaxy")
+    //  //cy.get('.App-header p').should('have.text', '2023-04-05')
+    //  cy.get('.App-header img').find(nasaResponse.url)
+    // })
+
+    // beforeEach(() => {
+    //   cy.visit('http://localhost:3000/mars')
+    // })
+    // it('look for mars', () => {    
+    //  cy.url().should('include', '/mars')
+    // })
+    
 })
