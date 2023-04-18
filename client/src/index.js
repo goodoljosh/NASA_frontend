edbce570653121c69f421d44281c68713bd69613
goodoljosh <findley_joshua1@columbusstate.edu>
2023-04-18 22:09:03 +0000

    Removed comments from index.js

--- client/src/index.js
+++ client/src/index.js
@@ -12,16 +12,11 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
-        {/* <Route path ="/home/:userId" /> */}
         <Route path ="pod" element = {<PictureoftheDay/>} />
          <Route path="mars" element={<MarsPhotos/>} />
-        {/* <Route path="about" element={<About />} /> */ }
       </Route>
     </Routes>
   </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
