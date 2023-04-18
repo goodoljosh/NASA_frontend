8c0e5f365a95c4fbaeae7ad40873d3241a253950
goodoljosh <findley_joshua1@columbusstate.edu>
2023-04-18 20:32:15 +0000

    Formatted Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,22 +1,19 @@
 import React from "react";
 
-
-
-const Header = (props)=> {
-
+const Header = (props) => {
 
     const nasaResponse = props.nasaResponse
-        return(
-            <header className="App-header">
-         <h1>
-          {nasaResponse.title}
-        </h1>
-        <p>{nasaResponse.date}</p>
-        <img src={nasaResponse.url} className ={"responsive"} width ={"600"} height = {"600"}/> 
+    return (
+        <header className="App-header">
+            <h1>
+                {nasaResponse.title}
+            </h1>
+            <p>{nasaResponse.date}</p>
+            <img src={nasaResponse.url} className={"responsive"} width={"600"} height={"600"} />
 
-         <p>{nasaResponse.explanation}</p>
-            </header>
-        )
+            <p>{nasaResponse.explanation}</p>
+        </header>
+    )
 }
 
 export default Header
