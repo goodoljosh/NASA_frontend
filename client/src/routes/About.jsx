0cb2dc5c61a56a43268a24c06f1d5eba22eb407d
goodoljosh <findleyathome@gmail.com>
2024-07-04 20:03:54 +0000

    Edited about description.

--- client/src/routes/About.jsx
+++ client/src/routes/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
       
       </header>
       <body>
-        This is an application made by Josh Findley and Brittney Johnson. The project calls two different API, a picture of the day and mars rover photos api, and displays the data for each on seperate pages of the application.
+        This is an application made by Josh Findley. The project calls two different API, a picture of the day and mars rover photos api, and displays the data for each on seperate pages of the application.
       </body>
       <footer>About Page</footer>
     </>
